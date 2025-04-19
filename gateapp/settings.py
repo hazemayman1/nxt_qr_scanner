85bf15dadd16fdd306b68cd240531afc53d5845a
Hazem Ayman <hazem.abulhassan@gmail.com>
2025-04-19 17:00:50 +0200

    settings for deployment

--- gateapp/settings.py
+++ gateapp/settings.py
@@ -99,11 +99,13 @@ WSGI_APPLICATION = 'gateapp.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+import os
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'data', 'db.sqlite3'),
     }
 }
 
