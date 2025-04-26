34e5093838d9d6ff6e0405aa45d733fb33e1a623
Hazem Ayman <hazem.abulhassan@gmail.com>
2025-04-26 21:41:51 +0300

    script and command to generate users and qr codes

--- qrapp/management/commands/generate_qr.py
+++ qrapp/management/commands/generate_qr.py
@@ -6,16 +6,11 @@ from urllib.parse import urlencode
 class Command(BaseCommand):
     help = 'Generate a QR code for a frontend link with user ID'
 
-    def add_arguments(self, parser):
-        pass
-        # parser.add_argument('--user_id', type=int, required=True, help='User ID to encode in QR code')
-        # parser.add_argument('--base_url', type=str, default='https://your-frontend.com/attendee', help='Frontend base URL')
-
     def handle(self, *args, **options):
-        attendees = Attendee.objects.all()
+        attendees = Attendee.objects.filter(is_vip = True).exclude(ptr_id__in = [1001, 987,654])
 
         for emp in attendees:
-            link = "https://example.com/some-page/" + str(emp.ptr_id)
+            link = f"https://qr-scanner-frontend-psi.vercel.app/scan?user_id={emp.ptr_id}"
             qr = qrcode.QRCode(
                 version=1,  # controls size of the QR Code
                 error_correction=qrcode.constants.ERROR_CORRECT_L,  # L, M, Q, H
@@ -26,4 +21,4 @@ class Command(BaseCommand):
             qr.make(fit=True)
 
             img = qr.make_image(fill="black", back_color="white")
-            img.save("qrapp/qrcodes/"+emp.name.replace(" ", "") + ".png")
+            img.save("qrapp/qrcodes_vip/"+emp.name.replace(" ", "") + ".png")
